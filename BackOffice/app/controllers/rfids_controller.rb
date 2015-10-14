fb2d0ddd3b61e4faf3ea1dc2464d3780a0cf0df3
fdiogo <diogoff94@gmail.com>
2015-10-14 19:56:35 +0100

    - Added an edit page for a rfid

--- BackOffice/app/controllers/rfids_controller.rb
+++ BackOffice/app/controllers/rfids_controller.rb
@@ -2,4 +2,12 @@ class RfidsController < ApplicationController
     def index
         @rfids = Rfid.all
     end
+
+    def update
+
+    end
+
+    def show
+        @rfid = Rfid.find_by number: params[:id]
+    end
 end
